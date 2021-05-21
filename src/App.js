1d068209d14e7da998bbe4aeb26d900923ea7127
hibamouhoub <hibamouhoub0@gmail.com>
2021-05-21 23:31:09 +0200

    intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav id="nav">
+        <a className="first-text" href="#home">She/Her</a>
+    </nav>
+
+    <section id="home">
+        <div className="title-group">
+            <h1>Hello World!</h1>
+            <small>Welcome to my website</small><br/>
+            <button className="resume">my resumé</button>
+        </div>
+        <div className="title-group">
+            <a href="https://github.com/hibamouhoub/"><i className="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/hibamouhoub/"><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://www.instagram.com/powerinz/"><i className="fab fa-instagram"></i></a>
+        </div>
+    </section>
+
     </div>
   );
 }
